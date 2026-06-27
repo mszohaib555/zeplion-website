04e4222e714522489c5d969f225fbbbe97036e7e
mszohaib <mszohaib2003@gmal.com>
2026-06-27 10:11:00 +0500

    Fixed Microsoft Clarity component

--- components/MicrosoftClarity.tsx
+++ components/MicrosoftClarity.tsx
@@ -3,11 +3,21 @@ import { useEffect } from 'react';
 
 export default function MicrosoftClarity() {
   useEffect(() => {
-    (function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window,document,"clarity","script","YOUR_CLARITY_ID");
+    (function(
+      c: Record<string, unknown>,
+      l: Document,
+      a: string,
+      r: string,
+      i: string
+    ){
+      const w = c as Record<string, ((...args: unknown[]) => void) & { q?: unknown[] }>;
+      w[a] = w[a] || function(...args: unknown[]){ (w[a].q = w[a].q || []).push(args) };
+      const t = l.createElement(r) as HTMLScriptElement;
+      t.async = true;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      const y = l.getElementsByTagName(r)[0];
+      y.parentNode?.insertBefore(t, y);
+    })(window as unknown as Record<string, unknown>, document, "clarity", "script", "xd3xwhkb2s");
   }, []);
   return null;
 }
